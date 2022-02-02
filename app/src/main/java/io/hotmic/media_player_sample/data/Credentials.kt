9e416b4e35d507e7f0660b907c919f27c551a8e7
jeffchandler30 <jeffchandle@gmail.com>
2022-02-02 09:11:01 -0800

    Clearing credentials

--- app/src/main/java/io/hotmic/media_player_sample/data/Credentials.kt
+++ app/src/main/java/io/hotmic/media_player_sample/data/Credentials.kt
@@ -3,8 +3,7 @@ package io.hotmic.media_player_sample.data
 class Credentials {
 
     companion object {
-        val API_KEY = "d34c0ae5-185b-4c3b-a55c-2d7ef9ebf5b6"
-        val PLATFORM_TOKEN =
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6eyJ1c2VyX2lkIjoiM2MyZGU3M2UtNmRiNi00MTg5LWJmY2EtMmE3NGNkN2Y3NjQ0IiwiZGlzcGxheV9uYW1lIjoiR2FuZ2EiLCJwcm9maWxlX3BpYyI6Imh0dHBzOi8vaG90bWljLWNvbnRlbnQuczMtdXMtd2VzdC0xLmFtYXpvbmF3cy5jb20vcHJvZmlsZV9waWNzLzNjMmRlNzNlLTZkYjYtNDE4OS1iZmNhLTJhNzRjZDdmNzY0NF8yODEuanBlZyJ9LCJpYXQiOjE2NDI0MzA5NTksImV4cCI6MTY0MjUxNzM1OX0.UEn0uwj2vkF5ircjjeO8QC0_HLvFNiCGDNQTfBTjcHU"
+        val API_KEY = "<< TODO: API KEY >>"
+        val PLATFORM_TOKEN = "<< TODO: JWT TOKEN >>"
     }
 }
